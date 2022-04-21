31863cf6d0a08ae4696c6a05857a7353b5e7e583
Lance-Nichols <32449269+Lance-Nichols@users.noreply.github.com>
2022-04-21 19:58:43 +1000

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+$(function() {
+    var marqueeClass = ".marquee__content";
+    var $tickerText = $(marqueeClass).children();
+    $tickerText.clone().appendTo(marqueeClass);
+    $tickerText.clone().appendTo(marqueeClass);
+  });
+  $(function() {
+    var marqueeClass2 = ".marquee__content2";
+    var $tickerText = $(marqueeClass2).children();
+    $tickerText.clone().appendTo(marqueeClass2);
+    $tickerText.clone().appendTo(marqueeClass2);
+  });
