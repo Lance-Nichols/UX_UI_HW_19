21e9ff36280fb8588efc84d2787708aa43080dc3
Lance-Nichols <32449269+Lance-Nichols@users.noreply.github.com>
2022-05-02 17:42:14 +1000

    code update

--- js/index.js
+++ js/index.js
@@ -90,3 +90,18 @@ $(document).ready(function(){
       $('.menu').toggleClass('active');
   });
 });
+
+$(".navMenuBars").on("click", function(){
+  $(".navMenuBars").toggleClass("active");
+  if ($(".navMenuBars").hasClass("active")) {
+    $(".navMenuBars").removeClass("fa-bars").addClass("fa-times");
+  }
+  else {
+    $(".navMenuBars").removeClass("fa-times").addClass("fa-bars");
+  }
+});
+
+$(".toggle").click(function(){
+  $(".toggle").addClass("active");
+});
+ 
